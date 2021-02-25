99679bc79bdcff52ec198e5c67574bdafb980ca5
seryozhabaleyko <seryozha.baleyko@gmail.com>
2021-02-25 11:42:46 +0300

    Fix localization

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+import { i18n } from '@tager/admin-services';
+
+import EN from './locales/en';
+import RU from './locales/ru';
+
+i18n.addTranslations('en', 'settings', EN);
+i18n.addTranslations('ru', 'settings', RU);
+
 export * from './constants/routes';
 export * from './constants/paths';
 export * from './utils/paths';
